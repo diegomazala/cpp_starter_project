4d1933162f3fceadd72693318c82f450ff0c468c
Diego Mazala @avell <diegomazala@gmail.com>
2018-09-19 19:16:40 -0300

    Fix typo

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     // A c++14 hello World
-    [out = std::ref(std:;cout << "Hello ")]() { out.get() << "Worlds\n"; } 
+    [out = std::ref(std::cout << "Hello ")]() { out.get() << "Worlds\n"; } 
     ();
 }
 
